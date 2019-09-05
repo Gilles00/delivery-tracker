7f96ff1687424f1a4f3a519d564ba179422c77c7
vladislavglad <vladglad.996@gmail.com>
2019-09-04 23:12:20 -0400

    2nd version

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 
 from bs4 import BeautifulSoup
 import requests
-#import time
+import time
 
-#traking_id = '' //through USPS
+#traking_id = '123' #through USPS
 traking_id = input('Please provide your tracking id: ')
 url = 'https://tools.usps.com/go/TrackConfirmAction?tLabels='
 
@@ -23,25 +23,34 @@ while True:
     print(test_url)
 '''
 
-#create session to pass "header" info.
-s = requests.Session()
-s.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-r = s.get(f'{url}{traking_id}')
-#print(r.status_code)
+def scrape():
+    #create session to pass "header" info.
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    r = s.get(f'{url}{traking_id}')
+    #print(r.status_code)
 
-soup_obj = BeautifulSoup(r.text, 'html.parser')
-#print(soup_obj.prettify())
+    soup_obj = BeautifulSoup(r.text, 'html.parser')
+    #print(soup_obj.prettify())
 
-'''grabs whole "div" class that we are interested in.
-delivery_status = soup_obj.find('div', class_='delivery_status')
-print(delivery_status)
-'''
+    '''grabs whole "div" class that we are interested in.
+    delivery_status = soup_obj.find('div', class_='delivery_status')
+    print(delivery_status)
+    '''
 
-#grabs specific item we need.
-delivery_status = soup_obj.find('p', class_='important').text
+    #grabs specific item we need.
+    return soup_obj.find('p', class_='important').text
 
-#check if deliverd
-if delivery_status.lower().find('delivered') != -1:
-    print('your item has been ' + delivery_status.lower())
-else:
-    print('your package is not delivered...')
+def is_delivered():
+    delivery_status = scrape()
+
+    if delivery_status.lower().find('delivered') != -1:
+        print('your package has been ' + delivery_status.lower())
+        return True
+    else:
+         print('your package is not delivered...')
+         return False
+
+#check every minute if "delivered"
+while not is_delivered():
+    time.sleep(60)
